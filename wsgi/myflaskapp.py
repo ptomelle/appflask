841601b4039128eb79fccb297c16a2233362fd4d
paoloT <paolo.tomelleri@gmail.com>
2016-04-11 03:32:45 +0200

    vrsione incompleta, manca solo app.js da completare

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask,request, send_from_directory
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route("/index.html")
+def hello1():
+    return send_from_directory('.', 'index.html')
+
+@app.route("/index.html")
+def hello2():
+    return send_from_directory('/static/', 'index1.html')
+
 
 if __name__ == "__main__":
     app.run()
