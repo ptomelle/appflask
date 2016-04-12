6d68d417d3a2e2a0c59a12a3ee6357c2636dd2e5
paoloT <paolo.tomelleri@gmail.com>
2016-04-12 02:17:10 +0200

    vrsione completa

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -11,9 +11,25 @@ def hello():
 def hello1():
     return send_from_directory('.', 'index.html')
 
-@app.route("/index.html")
+@app.route("/index1.html")
 def hello2():
     return send_from_directory('/static/', 'index1.html')
+
+@app.route("/elaboraReq1")
+def  elaboraReq1():
+	#  variabile var1, non la stringa 'var1'
+	var1 = request.args.get('var1')
+	#  variabile var2, non la stringa 'var2'
+	var2 = request.args.get('var2')
+
+        stringHtml= '<html><body>' + \
+            '<H3>' +request.query_string +'</H3><br/>' +\
+            '<H3>' + 'valore dei parametri ' +\
+           'var1=' + var1 +\
+              ' '+\
+            'var2=' + var2  +\
+	    '</H3></body></html>'
+        return stringHtml
 
 
 if __name__ == "__main__":
