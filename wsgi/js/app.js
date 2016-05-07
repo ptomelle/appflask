346ce9fc29135f106ec7381bed9b070633ed44ac
paoloT <paolo.tomelleri@gmail.com>
2016-05-07 22:53:58 +0200

    lettura dell'alunno 00 con metodo POST e inserimento con metodo GET prima di fare il contrario ( nella prossima versione minore )

--- wsgi/js/app.js
+++ wsgi/js/app.js
@@ -4,48 +4,81 @@
  * and open the template in the editor.
  */
 
-var PRODUCT_APP={
+var APP = {
     /*Returning jQuery Promise For a AJAX call with Product type*/
-    getProductDetails : function(type){
-          var ajaxRequest=$.ajax("ProductServlet?type="+type);
-          return ajaxRequest.promise();
-    },
+    // ...
     /*Handler For AJAX response*/
-    handleCallback : function(type){
-        var promise = PRODUCT_APP.getProductDetails(type);
-        promise.done(function(data){
-            PRODUCT_APP.doProductRendering(data);
+    // ...
+    /*jQuery Template building with JSON data  from Server*/
+    // ...
+    /*Event Listener per il  tag h3 con id h3_1,
+     * associa una funzione anonima alla gestione evento*/
+    init_h3_1_Click: function () {
+        $("#h3_1").on('click', function () {
+            // copia il contenuto di h3_1 in h3_2
+            // 
+            // trova questo h3
+            var contenuto = $("#h3_1").html();
+            elementoSelezionato2 = $("#h3_2")
+            elementoSelezionato2.html(contenuto);
         });
     },
-    /*jQuery Template building with JSON data  from Server*/
-    doProductRendering: function(data){
-        var productContainer =$('.ts-product-container'),
-            aProductTemplate = $('#aProductTemplate').tmpl( data )
-            /*,
-            promiseOldPro = $(productContainer).find('.panel').fadeOut().promise(); 
-            $.when(promiseOldPro).then(function(){
-            */
-            productContainer.html(aProductTemplate);
-        //});
+    init_h3_2_Click: function () {
+        elementoSelezionato = $("#h3_2")
+        elementoSelezionato.on('click', function () {
+            // copia il contenuto di h3_1 in h3_2
+            // trova questo h3
+            $("#h3_2").html("");
+        });
     },
-    /*Event Listener per il  tag h3 con id h3_1*/
-    init_h3_1_Click:function(){
-        $("").on('click','li',function(e){
-            e.preventDefault();
-            var li = e.currentTarget,
-                type= $(li).attr('data-category');
-            $(li).siblings('li').removeClass('active');
-            $(li).addClass('active');
-            PRODUCT_APP.handleCallback(type);
+    //  associa event click su button leggiCampoText1
+    init_leggiCampoText1_click: function () {
+        elementoSelezionato = $('#leggiCampoText1');
+        elementoSelezionato.on('click', function ()
+        {
+            /* copia dal campotesto */
+            elementoSelezionato2 = $("#campoText1");
+            contenuto = elementoSelezionato2.val();
+            /* chiamata asincrona, non aspetta la fine */
+            $.ajax(
+                    {   method: "POST",
+                        url :"/alunnoByNumeroReg/",
+                        contentType: 'application/json',
+                        crossDomain: true,   
+                        type: "json",
+                        data: JSON.stringify({"numeroReg": contenuto}),
+                        dataType: "json",
+                        success: function (data) {
+                            contenuto = data.numeroReg + " " +
+                            data.nome + " " +
+                            data.cognome + " " +
+                            data.annoNascita;
+                            // oggetto arrivato json in data
+                            elementoSelezionato = $("#campoText1-div");
+                            elementoSelezionato.html(contenuto);
+                        },
+                        error: function (data) {
+                            elementoSelezionato = $("#campoText1-div");
+                            elementoSelezionato.html("errore");
+                        }
+               });
+
+
+
         });
     }
 };
 
-$(document).ready(function(){
-    /*Initial Load Call Books */
-   /* PRODUCT_APP.handleCallback('book'); */
+
+//  LISTENER che fa partire il programma,
+//  si aspetta che il document sia pronto => tutti i .js .css ecc.  caricati
+
+$(document).ready(function () {
+
     /*Initialize Click Of Menu Item*/
-   PRODUCT_APP.initCategoryClick();
+    APP.init_h3_1_Click();
+    APP.init_h3_2_Click();
+    APP.init_leggiCampoText1_click();
 });
 
 
--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -1,8 +1,13 @@
 from flask import Flask,request, send_from_directory
 from flask import render_template
+from flask import jsonify
+from flask.ext.cors import CORS
+
 
 app = Flask(__name__)
+CORS(app)
 
+registroAlunni = {0:{"numeroReg":0,"nome":"ignoto","cognome":"ignoto","annoNascita":"1900"}}
 
 @app.route("/")
 def hello():
@@ -12,57 +17,37 @@ def hello():
 def hello1():
     return send_from_directory('.', 'index.html')
 
-@app.route("/index1.html")
-def hello2():
-    return send_from_directory('/static/', 'index1.html')
+@app.route("/js/<nomeFileJs>")
+def jsLoad(nomeFileJs):
+    return send_from_directory('js', nomeFileJs)
 
-@app.route("/elaboraInputHtml")
-def elaboraInputHtml():
-     #  variabile var1, non la stringa 'var1'
-	var1 = request.args.get('firstname')
-	#  variabile var2, non la stringa 'var2'
-	var2 = request.args.get('lastname')
-
-        stringHtml= '<html><body>' + \
-            '<H3>' +request.query_string +'</H3><br/>' +\
-            '<H3>' + 'valore dei parametri ' +\
-           'firstname=' + var1 +\
-              ' '+\
-            'lastname=' + var2  +\
-	    '</H3></body></html>'
-        return stringHtml
-    
-@app.route("/elaboraInputJson")
-def elaboraInputJson():
-    # recupera i valori spediti dalla queryString
-    var1 = request.args.get('firstname')
-	#  variabile var2, non la stringa 'var2'
-    var2 = request.args.get('lastname')
-    
-    stringJSON = ' { "firstName" :  ' +\
-                  '"' +    var1 +  '"'+\
-                 '   "lastName"  :  ' +\
-                  '"' +    var2 +  '"'+\
-                 '}'
-                 
-    return stringJSON
+@app.route("/css/<nomeFileCss>")
+def cssLoad(nomeFileCss):
+    return send_from_directory('css', nomeFileCss)
    
-@app.route("/elaboraInputJson_1")
-def elaboraInputJsontml():
-    # recupera i valori spediti dalla queryString
-    var1 = request.args.get('firstname')
-	#  variabile var2, non la stringa 'var2'
-    var2 = request.args.get('lastname')
+@app.route("/insertAlunno/")  # metodo GET per chiamare dalla barra del browser
+def inserisciAlunno ():
+    # spedizione in formato html
+    numeroReg =  request.args.get('numeroReg')
+    nome =       request.args.get('nome')
+    cognome =    request.args.get('cognome')
+    annoNascita =request.args.get('annoNascita') 
+    dizAlunno = { "numeroReg": numeroReg, "nome": nome,
+                  ":cognome" : cognome , "annoNascita":annoNascita}
+    registroAlunni[int(numeroReg)]= dizAlunno
+    return ""   #restituisce status = 200  OK , ma nessuna stringa
     
-    dizVar = {"firstname": var1, "lastname":var2}
     
-    return render_template('elaboraInputJson.json.tml', name = dizVar)
+@app.route("/alunnoByNumeroReg/", methods=["POST"]) # metodo POST
+def alunnoByNumeroReg():
+    # spedizione in formato html
     
-   
+    numeroReg =  request.json['numeroReg']
     
+    dizAlunno = registroAlunni[int(numeroReg)]
+    # in casi piu' complessi usare render_templates e quindi jsonify
+    return jsonify( ** dizAlunno)   #aggiunge content-type => json
     
-   
-
 if __name__ == "__main__":
-    app.debug=True
-    app.run(port=7000)
+    #app.debug=True
+    app.run(port=4000)
