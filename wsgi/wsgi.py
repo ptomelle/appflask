a84bff0c5c077bdc975c38652f6c0b3514a8fdaf
paoloT <paolo.tomelleri@gmail.com>
2016-05-10 21:55:54 +0200

    cambiata porta dad 4001 a 80 per vedere se è un errore su openshift

---
+++ wsgi/wsgi.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+from myflaskapp import app as application
